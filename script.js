3bd4b541e590e38ed3e698c53edd116bd1ede274
Jackson Hill <43260963+jacksonhill-77@users.noreply.github.com>
2024-02-11 20:49:15 +1100

    Create Book constructor, addBook function, and library array

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const myLibrary = []
 
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author; 
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  const title = prompt('Please enter the title of the book you wish to add to the library: ');
+  const author = prompt('Please enter the author of the book you wish to add to the library: ');
+  const pages = prompt('Please enter the amount of pages of the book you wish to add to the library: ');
+  const book = new Book(title, author, pages);
+  myLibrary.push(book);
+}
