e7b5633e62263341556578e56126051edde5815c
Jackson Hill <43260963+jacksonhill-77@users.noreply.github.com>
2024-02-20 19:15:35 +1100

    Add functions for displaying books as card divs

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = []
+const bookInput = document.getElementById("book-input");
+bookInput.addEventListener("click", addBookToLibrary);
 
 function Book(title, author, pages) {
   this.title = title;
@@ -12,4 +14,36 @@ function addBookToLibrary() {
   const pages = prompt('Please enter the amount of pages of the book you wish to add to the library: ');
   const book = new Book(title, author, pages);
   myLibrary.push(book);
+}
+
+function addBookInfoToDiv(book, div) {
+  let bookTitle = document.createElement("h4");
+  bookTitle.classList.add("title");
+  bookTitle.textContent = book.title;
+  let bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("body");
+  bookAuthor.textContent = book.author;
+  let bookPages = document.createElement("p");
+  bookPages.classList.add("body");
+  bookPages.textContent = book.pages;
+
+  div.appendChild(bookTitle);
+  div.appendChild(bookAuthor);
+  div.appendChild(bookPages);
+}
+
+function addBookDivToContainer(book, container) {
+  let bookElement = document.createElement("div");
+  bookElement.classList.add("card");
+
+  addBookInfoToDiv(book, bookElement);
+  container.appendChild(bookElement);
+}
+
+function addBooksToContainer() {
+  const container = document.getElementById("container");
+  
+  for (const book in myLibrary) {
+    addBookDivToContainer(book, container)
+  }
 }
