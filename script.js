a2c3cb92a662523395ac3703b2d6ac7be9ba6eff
jacksonhill-77 <contactjacksonhill@gmail.com>
2024-03-04 10:38:52 +1100

    Add dynamic card styling and fix book insertion logic

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const myLibrary = []
-const bookInput = document.getElementById("book-input");
-bookInput.addEventListener("click", addBookToLibrary);
-
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author; 
-  this.pages = pages;
-}
-
 function addBookToLibrary() {
   const title = prompt('Please enter the title of the book you wish to add to the library: ');
   const author = prompt('Please enter the author of the book you wish to add to the library: ');
@@ -43,7 +33,30 @@ function addBookDivToContainer(book, container) {
 function addBooksToContainer() {
   const container = document.getElementById("container");
   
-  for (const book in myLibrary) {
+  for (const book of myLibrary) {
     addBookDivToContainer(book, container)
   }
 }
+
+
+
+const myLibrary = []
+const bookInput = document.getElementById("book-input");
+bookInput.addEventListener("click", addBookToLibrary);
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author; 
+  this.pages = pages;
+}
+
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 218);
+const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 324);
+const book3 = new Book("1984", "George Orwell", 328);
+const book4 = new Book("Pride and Prejudice", "Jane Austen", 432);
+const book5 = new Book("The Catcher in the Rye", "J.D. Salinger", 224);
+
+myLibrary.push(book1, book2, book3, book4, book5);
+addBooksToContainer()
+
+
